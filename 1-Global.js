59338fac2d298c90fb34de61e165528a6eb39b4e
babai005 <hi.suvankar@gmail.com>
2022-07-04 20:45:03 +0530

    first push

---
+++ 1-Global.js
@@ -0,0 +1,25 @@
+// Globals      NOT WINDOW
+
+//__dirname     Print current directory name
+//__filemame    Print current file name
+//require       require() is a function to use module names (CommonJS)
+//module        info about current module (file name)
+//process       info about the env where the program is being executed
+
+
+//======================================================================
+
+
+//console.log('global ==', global);
+
+// setTimeout(() => {
+//     console.log('called by setTimeout')
+// }, 5000)
+
+// setInterval(() => {
+//     console.log('called by setInterval')
+// }, 2000)
+
+
+//console.log('current directory name ==>', __dirname);
+console.log('current file name ==>', __filename);
